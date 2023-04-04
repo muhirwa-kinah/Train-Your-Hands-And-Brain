59f0302f019de9c1d9b85130857b760cd86cf915
muhirwa-kinah <muhirwasingizashekinah@gmail.com>
2023-04-04 23:14:16 +0200

    working with uppercase

---
+++ 0x00-JS_Basics/0x00-Variables/0x02-up_or_down.js
@@ -0,0 +1,4 @@
+const BIRTHDAY = '18.04.1982'; 
+
+const age = someCode(BIRTHDAY);
+In contrast, age is evaluated in run-time. Today we have one age, a year after we’ll have another one. It is constant in a sense that it does not change through the code execution. But it is a bit “less of a constant” than birthday: it is calculated, so we should keep the lower case for it.
