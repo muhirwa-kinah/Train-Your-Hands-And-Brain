8b1e218f06a2411dd15722516ca2a28200167244
muhirwa-kinah <muhirwasingizashekinah@gmail.com>
2023-04-08 17:58:05 +0200

    math

---
+++ 3/math.mjs
@@ -0,0 +1,25 @@
+/**
+ * Lets do some math!
+ * Some rules first:
+ *   Enter the operations, replacing the `"?"`, make the computer do the work for you.
+ *   Do not manually enter the answer, for example: "one plus ten" would look like 1+10
+ *
+ * a = one plus eight
+ * b = 22 times three
+ * c = the *remainder* of 5/4
+ * d = the variable 'a' minus 17
+ * e = the sum of the previous four variables
+ */
+
+export const a = 1 + 8;
+export const b = 22 * 3;
+export const c = 5 % 4;
+export const d = a - 17;
+export const e = a + b + c + d;
+
+// export default { a, b, c, d, e };
+// module.exports = a;
+// module.exports = b;
+// module.exports = c;
+// module.exports = d;
+// module.exports = e;
